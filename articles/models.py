32735be709691436ac293d69346161df96482aca
cferguson <chris.t.ferguson@gmail.com>
2016-03-20 13:40:34 -0700

    Created first app

---
+++ articles/models.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+#class Article(models.Model):
+#	title = models.CharField(max_length=255, null=False, blank=False)
+#	slug = models.SlugField(max_length=255, null=False, blank=False)
+#	text = models.TextField(null=False, blank=False)
