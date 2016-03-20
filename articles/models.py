71b8a194f9c24fe7e8ef0812cc41a5a45bc4d64a
cferguson <chris.t.ferguson@gmail.com>
2016-03-20 14:02:11 -0700

    Added models

--- articles/models.py
+++ articles/models.py
@@ -3,7 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-#class Article(models.Model):
-#	title = models.CharField(max_length=255, null=False, blank=False)
-#	slug = models.SlugField(max_length=255, null=False, blank=False)
-#	text = models.TextField(null=False, blank=False)
+class Article(models.Model):
+	title = models.CharField(max_length=255, null=False, blank=False)
+	slug = models.SlugField(max_length=255, null=False, blank=False)
+	text = models.TextField(null=False, blank=False)
+	created = models.DateTimeField(auto_now_add =True, null=False)
+	modified = models.DateTimeField(auto_now=True, null=False)
+
+
+class ArticleView(models.Model):
+	article = models.ForeignKey(Article, null=False)
+	created = models.DateTimeField(auto_now_add =True, null=False)
